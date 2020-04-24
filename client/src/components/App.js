07312c26607feeea66a2c352255b7fb52dad24c7
George Luis Pagarigan <georgeluis.pagarigan@infor.com>
2020-04-24 22:53:34 +0800

    fix for jsx issue

---
+++ client/src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from "../images/Team.png";
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My React App!</h1>
+                <img src={logo} />
+                <div className="">
+
+
+
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default App;
